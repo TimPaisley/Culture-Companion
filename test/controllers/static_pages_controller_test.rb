3be9d4eb1b6d58fe8b0cca9c404b10fa4d6929e1
TimPaisley <tepaisley@gmail.com>
2016-04-29 23:08:24 +1200

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_select "title", "About | Culture Companion"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Culture Companion"
+  end
+
 end
