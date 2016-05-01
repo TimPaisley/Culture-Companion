8dab0b4930833bbab4833157d32237dbaee49c27
TimPaisley <tepaisley@gmail.com>
2016-05-01 23:24:34 +1200

    Finished basic styling

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,10 +19,4 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_select "title", "About | Culture Companion"
   end
 
-  test "should get contact" do
-    get :contact
-    assert_response :success
-    assert_select "title", "Contact | Culture Companion"
-  end
-
 end
